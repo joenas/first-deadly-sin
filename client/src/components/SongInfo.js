76831b665a4c009772fc626f2de00df7367d280d
joenas <jon@jonnev.se>
2020-03-20 13:09:30 +0100

    Mobile improvements

--- client/src/components/SongInfo.js
+++ client/src/components/SongInfo.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-function SongInfo({ mpdInfo: { artist, album, title } }) {
+function SongInfo({ mpdInfo: { artist, title } }) {
   if (!(artist || title)) return null;
   return (
     <div>
       <h1>{title}</h1>
       <h2>{artist}</h2>
-      <h3>{album}</h3>
     </div>
   );
 }
